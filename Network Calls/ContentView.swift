237c500bafd28ee125ad39a55105a9b9ca91c5cc
Elaidzha1940 <eli080293@gmail.com>
2023-07-03 22:57:27 +0300

    1st commit

---
+++ Network Calls/ContentView.swift
@@ -0,0 +1,28 @@
+//  /*
+//
+//  Project: Network Calls
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 03.07.2023
+//
+//  S
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "ПИДОРАС")
+                
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Network Calls/Network_CallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_CallsApp.swift
+//  Network Calls
+//
+//  Created by Elaidzha Shchukin on 03.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Network_CallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
