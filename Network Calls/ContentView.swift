f28d5dc88b099e915b3f8a51a90051854890fcc1
Elaidzha1940 <eli080293@gmail.com>
2023-07-05 20:17:30 +0300

    it is almost done

--- Network Calls/ContentView.swift
+++ Network Calls/ContentView.swift
@@ -33,14 +33,26 @@ struct ContentView: View {
     }
     
     func getUser() async throws -> GitHubUser {
-    
+        
         let endpoint = "https://api.github.com/users/Elaidzha1940"
         
         guard let url = URL(string: endpoint) else {
             throw GHError.invalidURL
         }
-                
+        
         let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw GHError.invaliResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(GitHubUser.self, from: data)
+        } catch {
+            
+        }
     }
 }
 
@@ -53,11 +65,12 @@ struct ContentView_Previews: PreviewProvider {
 struct GitHubUser: Codable {
     
     let login: String
-    let avatar_url: String
+    let avatarUrl: String
     let bio: String
 }
 
 enum GHError: Error {
     
     case invalidURL
+    case invaliResponse
 }
